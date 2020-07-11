6b07309b3e704cfc03eb57222a4c3d295163cb80
Aljamil1113 <aarazasaljamil@gmail.com>
2020-07-11 15:03:58 -0700

    Add 3rd party components

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -9,22 +10,21 @@ import { AuthService } from '../_services/auth.service';
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter<boolean>();
   model: any = {};
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
   }
 
   register(){
     this.authService.register(this.model).subscribe(() => {
-      console.log('Registered Successfully!');
+      this.alertify.success('Registered Successfully!');
     }, error => {
-      console.log(error);
+      this.alertify.error(error);
     });
   }
 
   cancel(){
     this.cancelRegister.emit(false);
-    console.log('Cancelled!');
   }
 
 }
--- README.md
+++ README.md
@@ -6,3 +6,11 @@ dotnet tool install --global dotnet-ef
 
 #Decription of token
 https://jwt.io/
+
+3rd Parties Component:
+    @auth0/angular-jwt": "^5.0.1",
+    "alertifyjs": "^1.13.1",
+    "bootstrap": "^4.5.0",
+    "bootswatch": "^4.5.0",
+    "font-awesome": "^4.7.0",
+    "ngx-bootstrap": "^4.3.0",
