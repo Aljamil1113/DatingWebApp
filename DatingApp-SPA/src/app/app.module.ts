bc7f244236bc7430e4b5f31ba3f018e94742a9eb
Aljamil1113 <aarazasaljamil@gmail.com>
2020-07-17 23:55:53 -0700

    User Extension UI

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,21 +2,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule } from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-
-import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { AuthService } from './_services/auth.service';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-import { ErrorInterceptorProvider } from './_services/error.interceptor';
-import { AlertifyService } from './_services/alertify.service';
-import { ListsComponent } from './lists/lists.component';
-import { MemberListComponent } from './member-list/member-list.component';
-import { MessagesComponent } from './messages/messages.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
 import { AuthGuard } from './_guards/auth.guard';
+import { UserService } from './_services/user.service';
+
+import { ErrorInterceptorProvider } from './_services/error.interceptor';
+import { AuthService } from './_services/auth.service';
+import { JwtModule } from '@auth0/angular-jwt';
+
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { AlertifyService } from './_services/alertify.service';
+import { ListsComponent } from './lists/lists.component';
+import { MemberListComponent } from './members/member-list/member-list.component';
+import { MessagesComponent } from './messages/messages.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { MemberDetailedComponent } from './members/member-detailed/member-detailed.component';
+import { MemberDetailedResolver } from './_resolvers/member-detailed.resolver';
+import { MemberListResolver } from './_resolvers/member-list.resolver';
+
+
+export function tokenGetter(){
+   return localStorage.getItem('token');
+}
 
 @NgModule({
    declarations: [
@@ -26,20 +41,33 @@ import { AuthGuard } from './_guards/auth.guard';
       RegisterComponent,
       ListsComponent,
       MemberListComponent,
-      MessagesComponent
+      MessagesComponent,
+      MemberCardComponent,
+      MemberDetailedComponent
    ],
    imports: [
       BrowserModule,
       HttpClientModule,
       FormsModule,
       BsDropdownModule.forRoot(),
-      RouterModule.forRoot(appRoutes)
+      TabsModule.forRoot(),
+      RouterModule.forRoot(appRoutes),
+      JwtModule.forRoot({
+         config: {
+            tokenGetter: tokenGetter,
+            allowedDomains: ['localhost:5000'],
+            disallowedRoutes: ['localhost:5000/api/auth']
+         }
+      }),
+      NgxGalleryModule
    ],
    providers: [
       AuthService,
       ErrorInterceptorProvider,
       AlertifyService,
-      AuthGuard
+      AuthGuard, UserService,
+      MemberDetailedResolver,
+      MemberListResolver
    ],
    bootstrap: [
       AppComponent
--- README.md
+++ README.md
@@ -8,9 +8,16 @@ dotnet tool install --global dotnet-ef
 https://jwt.io/
 
 3rd Parties Component:
-    @auth0/angular-jwt": "^5.0.1",
+
     "alertifyjs": "^1.13.1",
     "bootstrap": "^4.5.0",
     "bootswatch": "^4.5.0",
     "font-awesome": "^4.7.0",
     "ngx-bootstrap": "^4.3.0",
+
+    @auth0/angular-jwt": "^5.0.1",
+    https://github.com/auth0/angular2-jwt
+
+
+    NgxGallery
+    https://www.npmjs.com/package/ngx-gallery
