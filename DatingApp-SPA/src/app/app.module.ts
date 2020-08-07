6e6df330c875a3488c37aa779ca7c167ecffa8df
AlArazas07 <aljamil.arazas@apptech-experts.com>
2020-08-06 22:43:07 -0700

    Like functionality

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -34,6 +34,7 @@ import { MemberEditResolver } from './_resolvers/member-edit.resolver';
 import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 import { PhotoEditorComponent } from './members/photo-editor/photo-editor.component';
 import { TimeAgoExtendsPipe } from './_pipes/TimeAgoExtendsPipe.pipe';
+import { ListsResolver } from './_resolvers/lists.resolver';
 
 
 
@@ -86,7 +87,8 @@ export function tokenGetter(){
       MemberDetailedResolver,
       MemberListResolver,
       MemberEditResolver,
-      PreventUnsavedChanges
+      PreventUnsavedChanges,
+      ListsResolver
    ],
    bootstrap: [
       AppComponent
